0631bdc872f30c0ee86b0181e45e95570c3e3e15
Rachel Lehn <rachel.lehn@yahoo.com>
2026-02-13 22:10:20 -0500

    making it look at least a bit human

--- projects/portrait/main.js
+++ projects/portrait/main.js
@@ -4,34 +4,28 @@
 
 	//setup the colors for fill and stroke
 	ctx.fillStyle="blue";
-	ctx.strokeStyle="#f5424e";
+	ctx.strokeStyle="#black";
 	ctx.lineWidth = 10;
 
-	//Draw the background
-	ctx.fillRect(300,500, 800, 800);
-
 	//arc(x,y,radius,start,stop)
-	ctx.fillStyle="lightblue";
-	ctx.beginPath();
-	ctx.lineTo(0,800);
-	ctx.lineTo(200,550);
+	ctx.fillStyle="blue";
+	ctx.fillRect(50,600,700,400);
+	ctx.fillRect(150,525,500,200);
 	//color it in
 	ctx.fill();
 	ctx.stroke();
 
-	ctx.fillStyle="orange"
-	ctx.beginPath();
-	ctx.arc(0,800,300,Math.PI,Math.PI/2);
-	ctx.arc(500,300,300,Math.PI/2,2*Math.PI);
-	ctx.fill();
 	//neck
-	ctx.fillStyle="green"
+	ctx.fillStyle="tan"
 	ctx.fillRect(310,450,175,120);
-
-	ctx.fillRect(300,300,100,100);
+	ctx.beginPath();
+	ctx.lineTo(310,570);
+	ctx.lineTo(400,700);
+	ctx.lineTo(485,570);
+	ctx.fill();
 	//elipse
 	ctx.beginPath();
-	ctx.fillStyle="green"
+	ctx.fillStyle="tan"
 	ctx.ellipse(
 		400,250, //x,y of the center
 		250, //radius along the x axis
@@ -40,11 +34,26 @@
 		0, 2*Math.PI //start, stop
 		)
 	ctx.fill();
+	//hair
+	ctx.fillStyle="#795548"
+	ctx.beginPath();
+	ctx.lineTo(200,150);
+	ctx.lineTo(575,150);
+	ctx.lineTo(600,700);
+	ctx.lineTo(775,700);
+	ctx.lineTo(700,300);
+	ctx.lineTo(600,1);
+	ctx.lineTo(200,1);
+	ctx.lineTo(100,300);
+	ctx.lineTo(50,700);
+	ctx.lineTo(250,700);
+	ctx.lineTo(250,100);
+	ctx.fill();
 	//eye
 	ctx.beginPath();
 	ctx.fillStyle="white"
 	ctx.ellipse(
-	320,185,
+	320,255,
 	60,
 	25,
 	0,
@@ -54,7 +63,7 @@ ctx.fill();
 	ctx.beginPath();
 	ctx.fillStyle="white"
 	ctx.ellipse(
-	520,185,
+	510,255,
 	60,
 	25,
 	0,
@@ -62,7 +71,18 @@ ctx.fill();
 	)
 ctx.fill();
 	ctx.beginPath();
-	ctx.fillStyle="black"
-	ctx.arc(320,185,20,0,2*Math.PI);
+	ctx.fillStyle="blue"
+	ctx.arc(320,255,25,0,2*Math.PI);
 	ctx.fill();
-
+	ctx.beginPath();
+	ctx.fillStyle="black"
+	ctx.arc(320,255,15,0,2*Math.PI);
+	ctx.fill();
+ctx.beginPath();
+ctx.fillStyle="blue"
+ctx.arc(510,255,25,0,2*Math.PI);
+ctx.fill();
+ctx.beginPath();
+	ctx.fillStyle="black"
+	ctx.arc(510,255,15,0,2*Math.PI);
+	ctx.fill();
