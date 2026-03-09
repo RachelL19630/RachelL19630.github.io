30d5af61c3018d29045c587a1787578a499d2255
Rachel Lehn <rachel.lehn@yahoo.com>
2026-03-09 13:51:36 -0400

    adding these random files

---
+++ practice/else.js
@@ -0,0 +1,23 @@
+function addToBody(text){
+	document.body.innerHTML +="<p>" + text + "<p>";
+}
+"use strict";
+/*
+console.log("test");
+let yourName = prompt("What is your name?");
+
+addToBody("Hi "+yourName);
+*/
+let temperature = prompt("What is the temperature outside?");
+temperature = Number(temperature);
+addToBody("It's " + temperature + " degrees");
+if (temperature < 32){
+	addToBody("It's freezing! Stay inside");
+}
+else if(temperature > 80){
+	addToBody("Go to the beach");
+}
+else{
+	addToBody ("Go for a walk");
+}
+addToBody("Have a nice day!");
