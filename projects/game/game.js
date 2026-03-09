229cd9227b360a78d477b0b4625a7fe52495a704
Rachel Lehn <rachel.lehn@yahoo.com>
2026-03-09 15:00:18 -0400

    adding more to the game

--- projects/game/game.js
+++ projects/game/game.js
@@ -1,6 +1,7 @@
 let gameActive = true; //this variable is required. 
                        //to stop the game, set it to false.
-
+let haveskis = false;
+let havemeat = false;
 //Declare your other global variables here
 
 
@@ -8,40 +9,137 @@ let gameActive = true; //this variable is required.
 
 
 //Make one function for each location
-function Home() {
+function home() {
     clear();
     print("\nYou are at Home!");
-    print("\nWhere do you want to go next? Say one of these choices:" +
-        "\n\nGarage");
+    print("\nWhere do you want to go next? Type one of these choices:" +
+        "\n\tGarage" + "\n\tCar" + "\n\tMeadow");
     
     function processInput(input){
         if (input.toLowerCase() === "garage") {
-            locationB();
-        } else {
+            garage();
+        } 
+	    else if (input.toLowerCase() === "car"){
+		car();
+	}
+	    else if (input.toLowerCase() === "meadow"){
+		meadow();
+	}
+	    else {
             stayHere();
-            waitThenCall(Home);
+            waitThenCall(home);
         }
     }
     waitForInput(processInput);
 }
 
-function locationB() {
+function garage() {
     clear();
-    print("\nYou are in location B!");
-    print("\nWhere do you want to go next? Say one of these choices:" +
-        "\n\tlocationA");
+    print("\nYou are in the Garage!");
+	print("\nYou see big open Garage with what looks like a treadmill and weights on one side, and a dark coordidor on the other");
+    print("\nWhere do you want to go next? Type one of these choices:" +
+        "\n\tHome" + "\n\tWorkout Equipment" + "\n\tDark Coridor");
     
     function processInput(input){
-        if (input.toLowerCase() === "locationa") {
-            locationA();
-        } else {
+        if (input.toLowerCase() === "home") {
+            home();
+        } 
+	    else if(input.toLowerCase() === "workout equipment"){
+		workoutEquipment();
+	}
+	    else if (input.toLowerCase() === "dark corridor"){
+		darkCorridor();
+	}
+	    else {
             stayHere();
-            waitThenCall(locationB);
+            waitThenCall(garage);
         }
     }
     waitForInput(processInput);
 }
+function workoutEquipment(){
+	clear();
+	print("\nYou step closer to the workout equipment and notice that it is all stacked on top of each other");
+  setTimeout(function() {
+                print("...");
+                setTimeout(function() {
+                    print("...");
+		})
+  })
+	print("\nSuddenly the equipment shudders and falls, crushing you beneath it!");
+	death();		
+}
+function death(){
+	clear();
+print("\nYou died");
 
+        print("\nwould you like to try again? Say yes or no");
+        function processInput(input){
+            if (input.toLowerCase() === "yes") {
+                haveskis = false;
+                havemeat = false;
+                start();
+            } else if (input.toLowerCase() === "no") {
+                print("\nok, better luck next time!");
+                gameActive = false;
+            } else {
+                print("Please type 'yes' or 'no'");
+            }
+        }
+
+}
+function car() {
+    clear();
+    print("\nYou are at the Car");
+    print("\nWhere do you want to go next? Type one of these choices:" +
+        "\n\tGo Back Home");
+    
+    function processInput(input){
+        if (input.toLowerCase() === "go back home") {
+            home();
+        } else {
+            stayHere();
+            waitThenCall(car);
+        }
+    }
+    waitForInput(processInput);
+}
+function meadow() {
+    clear();
+    print("\nYou are in the meadow");
+    print("\nWhere do you want to go next? Type one of these choices:" +
+        "\n\tHome" +"\n\tBonfire");
+    
+    function processInput(input){
+        if (input.toLowerCase() === "home") {
+            home();
+        } 
+	    else if (input.toLowerCase() === "bonfire"){
+		bonfire();
+	}
+	    else {
+            stayHere();
+            waitThenCall(meadow);
+        }
+    }
+    waitForInput(processInput);
+}
+function bonfire() {
+    clear();
+    print("\nYou are at the Bonfire");
+    print("\nWhere do you want to go next? Type one of these choices:" +
+        "\n\tBack to the Meadow");
+    
+    function processInput(input){
+        if (input.toLowerCase() === "back to the meadow") {
+            meadow();
+        } else {
+            stayHere();
+            waitThenCall(locationA);
+        }
+    }
+    waitForInput(processInput);
+}
 //finally, make sure you customize this to tell it what should happen at the
 //very start. For this simple example, any input will bring you
 //to locationA
@@ -49,7 +147,7 @@ function start(){
     print("Welcome to my game! Press any key to start");
 
     function processInput(input){
-            locationA();
+            home();
     }
     waitForInput(processInput);
 }
